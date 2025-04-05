14c957f889f12b052321f1bfed45c49bcbd2397a
AhmedTarek <o0ahmedtarek0o@gmail.com>
2025-04-05 17:48:46 +0200

    adding app images

---
+++ lib/core/constants/app_images.dart
@@ -0,0 +1,5 @@
+abstract final class AppImages{
+  static const oImage = "assets/images/o_image.svg";
+  static const xImage = "assets/images/x_image.svg";
+  static const xOBackgroundImage = "assets/images/x_o_background.png";
+}
