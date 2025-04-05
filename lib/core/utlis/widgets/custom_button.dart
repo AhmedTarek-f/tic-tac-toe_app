dac7a4a5074da80cb96d5d2d243d273addb36d68
AhmedTarek <o0ahmedtarek0o@gmail.com>
2025-04-05 17:49:26 +0200

    adding custom button widget

---
+++ lib/core/utlis/widgets/custom_button.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:tic_tac_toe_app/core/constants/app_colors.dart';
+import 'package:tic_tac_toe_app/core/constants/app_fonts.dart';
+import 'package:tic_tac_toe_app/core/utlis/screen_size.dart';
+
+class CustomButton extends StatelessWidget {
+  const CustomButton({
+    super.key,
+    this.onPressed,
+    required this.buttonTitle,
+    this.style,
+    this.backgroundColor,
+  });
+
+  final String buttonTitle;
+  final void Function()? onPressed;
+  final TextStyle? style;
+  final Color? backgroundColor;
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(ScreenSize.getWidth(context)*0.0426)),
+        backgroundColor: backgroundColor ?? AppColors.white,
+      ),
+      onPressed: (){},
+      child: Text(
+        buttonTitle,
+        style: style ?? AppFonts.fontSize16Normal.copyWith(color: AppColors.black),
+      ),
+    );
+  }
+}
