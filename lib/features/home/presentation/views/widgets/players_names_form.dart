7f80c0088d4b6e671dbcc02e4f514f09cad8323c
AhmedTarek <o0ahmedtarek0o@gmail.com>
2025-04-06 15:21:46 +0200

    separating first and second names field to new files and adding logic

--- lib/features/home/presentation/views/widgets/players_names_form.dart
+++ lib/features/home/presentation/views/widgets/players_names_form.dart
@@ -1,29 +1,28 @@
 import 'package:flutter/material.dart';
-import 'package:tic_tac_toe_app/core/constants/app_fonts.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:tic_tac_toe_app/features/home/presentation/views/widgets/first_player_name_field.dart';
+import 'package:tic_tac_toe_app/features/home/presentation/views/widgets/second_player_name_field.dart';
+import 'package:tic_tac_toe_app/features/home/presentation/views_model/home_cubit.dart';
+import 'package:tic_tac_toe_app/features/home/presentation/views_model/home_state.dart';
 
 class PlayersNamesForm extends StatelessWidget {
   const PlayersNamesForm({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Form(
-        child: Column(
-          children: [
-            TextFormField(
-              style: AppFonts.fontSize16Normal,
-              decoration: const InputDecoration(
-                  hintText: "First player name"
-              ),
-            ),
-            const SizedBox(height: 16,),
-            TextFormField(
-              style: AppFonts.fontSize16Normal,
-              decoration: const InputDecoration(
-                  hintText: "Second player name"
-              ),
-            ),
-          ],
-        )
+    final controller = BlocProvider.of<HomeCubit>(context);
+    return BlocBuilder<HomeCubit,HomeState>(
+      builder:(context, state) => Form(
+          key: controller.playersFormKey,
+          autovalidateMode: controller.autoValidateMode,
+          child: const Column(
+            children: [
+              FirstPlayerNameField(),
+              SizedBox(height: 16,),
+              SecondPlayerNameField(),
+            ],
+          )
+      ),
     );
   }
 }
