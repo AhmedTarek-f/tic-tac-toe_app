7a4352290d39a0620da117ae635bc6606c2beb25
AhmedTarek <o0ahmedtarek0o@gmail.com>
2025-04-06 15:20:10 +0200

    adding bloc provider

--- lib/features/home/presentation/views/home_view.dart
+++ lib/features/home/presentation/views/home_view.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:tic_tac_toe_app/core/constants/app_colors.dart';
 import 'package:tic_tac_toe_app/features/home/presentation/views/widgets/home_view_body.dart';
+import 'package:tic_tac_toe_app/features/home/presentation/views_model/home_cubit.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
@@ -9,18 +11,20 @@ class HomeView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       decoration: const BoxDecoration(
-        gradient: LinearGradient(
-          colors: [
-            AppColors.cyan,
-            AppColors.blue,
-          ],
-          begin: Alignment.topCenter,
-          end: Alignment.bottomCenter
-        )
+          gradient: LinearGradient(
+              colors: [
+                AppColors.cyan,
+                AppColors.blue,
+              ],
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter
+          )
       ),
-      child: const Scaffold(
-        backgroundColor: Colors.transparent,
-        body: HomeViewBody(),
+      child:  Scaffold(
+        body: BlocProvider<HomeCubit>(
+          create: (context) => HomeCubit(),
+          child: const HomeViewBody(),
+        ),
       ),
     );
   }
