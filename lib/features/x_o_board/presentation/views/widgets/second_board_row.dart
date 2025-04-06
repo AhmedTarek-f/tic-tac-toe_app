c7a7e312b403ac902d0c8d65b8c9689bb1b63096
AhmedTarek <o0ahmedtarek0o@gmail.com>
2025-04-06 14:32:46 +0200

    adding second board row

---
+++ lib/features/x_o_board/presentation/views/widgets/second_board_row.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:tic_tac_toe_app/core/constants/app_colors.dart';
+import 'package:tic_tac_toe_app/features/x_o_board/presentation/views/widgets/clickable_x_o_button.dart';
+
+class SecondBoardRow extends StatelessWidget {
+  const SecondBoardRow({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Row(
+      children: [
+        Expanded(child: ClickableXOButton(xOrO: "x",)),
+        VerticalDivider(color: AppColors.black,),
+        Expanded(child: ClickableXOButton(xOrO: "o",)),
+        VerticalDivider(color: AppColors.black,),
+        Expanded(child: ClickableXOButton()),
+      ],
+    );
+  }
+}
