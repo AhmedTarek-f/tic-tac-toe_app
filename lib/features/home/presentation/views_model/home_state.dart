6147edc66afaecf43658943b9429a3cb462eba63
AhmedTarek <o0ahmedtarek0o@gmail.com>
2025-04-06 15:23:20 +0200

    adding home states

---
+++ lib/features/home/presentation/views_model/home_state.dart
@@ -0,0 +1,9 @@
+sealed class HomeState {}
+
+final class HomeInitialState extends HomeState {}
+
+final class ChangeGameModeState extends HomeState {}
+
+final class ChangeAutoValidateModeState extends HomeState {}
+
+final class ChangeGameStartWithXOROState extends HomeState {}
