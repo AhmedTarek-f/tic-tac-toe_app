9ab25bd73e9e10639076824844502b6e93371fa2
AhmedTarek <o0ahmedtarek0o@gmail.com>
2025-04-08 13:14:34 +0200

    adding X O Board state

---
+++ lib/features/x_o_board/presentation/views_model/x_o_board_state.dart
@@ -0,0 +1,9 @@
+
+sealed class XOBoardState {}
+
+final class XOBoardInitial extends XOBoardState {}
+
+final class ChangeBoardTileState extends XOBoardState {}
+
+final class ResetBoardTileState extends XOBoardState {}
+
