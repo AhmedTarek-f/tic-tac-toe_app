abca0053026f3a150cfc9dbad50fa576800ef4fd
AhmedTarek <o0ahmedtarek0o@gmail.com>
2025-04-08 13:16:44 +0200

    adding navigation to board view

--- lib/features/home/presentation/views/widgets/start_button.dart
+++ lib/features/home/presentation/views/widgets/start_button.dart
@@ -5,7 +5,9 @@ import 'package:tic_tac_toe_app/core/constants/app_fonts.dart';
 import 'package:tic_tac_toe_app/core/utlis/screen_size.dart';
 import 'package:tic_tac_toe_app/core/utlis/widgets/custom_button.dart';
 import 'package:tic_tac_toe_app/features/home/presentation/views_model/home_cubit.dart';
+import 'package:tic_tac_toe_app/features/x_o_board/data/models/player_model.dart';
 import 'package:tic_tac_toe_app/features/x_o_board/presentation/views/x_o_board_view.dart';
+import 'package:tic_tac_toe_app/features/x_o_board/presentation/views_model/x_o_board_cubit.dart';
 
 class StartButton extends StatelessWidget {
   const StartButton({
@@ -21,7 +23,23 @@ class StartButton extends StatelessWidget {
           controller.changeAutoValidateModeToDisabled();
           Navigator.of(context).pushReplacement(
             MaterialPageRoute(
-              builder: (context) => const XOBoardView(),
+              builder: (context) => BlocProvider<XOBoardCubit>(
+                create: (context) => XOBoardCubit(
+                    isStartWithX: controller.startWithX,
+                    firstPlayerData: PlayerModel(
+                        playerName: controller.firstPlayerNameController.text,
+                        playerScore: 0,
+                        oneWinnerGameSignsIndexes: controller.isNormalGameMode?null : [],
+                    ),
+                    secondPlayerData: PlayerModel(
+                        playerName: controller.secondPlayerNameController.text,
+                        playerScore: 0,
+                       oneWinnerGameSignsIndexes: controller.isNormalGameMode?null : [],
+                    ),
+                  isNormalGameMode: controller.isNormalGameMode
+                ),
+                child: const XOBoardView(),
+              ),
             ),
           );
         }
